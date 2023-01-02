d02be66b29d843a9a7ab10db59ce25fa9fed31c2
zhagynchik <zhagynchik@mail.ru>
2023-01-02 16:14:01 +0300

    First commit

---
+++ DZ9.1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"   */
+
+int GetNumber(string text)
+{
+    int result = 0;
+    Console.WriteLine(text);
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+string GetValues(int n)
+{
+    if (n==1)
+    {
+        return n.ToString();
+    }
+    return GetValues(n-1) + $", {n}";
+}
+
+int n = GetNumber("Введите число больше 1"); 
+
+GetValues(n);
+Console.WriteLine(GetValues(n));
+
